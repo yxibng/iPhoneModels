6aab9f7152a244a0689a70136527290e06ec8644
yxibng <yxibng@gmail.com>
2018-12-28 13:51:26 +0800

    Initial Commit

---
+++ DeviceMode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeviceMode
+//
+//  Created by yxibng on 2018/12/28.
+//  Copyright © 2018 yxibng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DeviceMode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DeviceMode
+//
+//  Created by yxibng on 2018/12/28.
+//  Copyright © 2018 yxibng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
